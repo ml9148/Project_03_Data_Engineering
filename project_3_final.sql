c0a6854ae42dab9db5a7f94a57ff14f54bc5a3e2
comfyben <bened.comfort@gmail.com>
2024-03-25 22:11:42 -0500

    SQL correctly displays pandas dataframes

--- project_3_final.sql
+++ project_3_final.sql
@@ -1,5 +1,6 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+CREATE DATABASE project_3_g1;
 
 -- DROP TABLE IF EXISTS host;
 
